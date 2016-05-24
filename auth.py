f8334f5039ac342ba0236d4ac61926ecc8d7e0a5
masha <maryged@gmail.com>
2016-05-24 12:36:35 -0400

    oauth cancer

---
+++ auth.py
@@ -0,0 +1,31 @@
+import urllib2
+import json
+
+error='DEFAULT'
+client_id='179236941327-flh4rlgnlgs9sh5u6ijnca3557ajei9o.apps.googleusercontent.com'
+def authenticate(id_token):
+    r=urllib2.urlopen('https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=%s' % id_token)
+    resp=json.load(r)
+    global error
+    if r.getcode()==200:
+        if resp['aud']==client_id:
+            if 'hd' in resp.keys():
+                if resp['hd']=='stuy.edu':
+                    return True
+            else:
+                error='Invalid domain.'
+        else:
+            error='Invalid client ID.'
+    else:
+        error='Invalid user token ID.'
+    return False
+
+def getName(id_token):
+    r=urllib2.urlopen('https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=%s' % id_token)
+    resp=json.load(r)
+    return resp['name']
+
+def getError():
+    global error
+    return error
+
