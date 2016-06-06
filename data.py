4093fd6f46a21a8d27cb99386def8313db0433cb
Sally <sallybao29@gmail.com>
2016-06-05 23:03:07 -0400

    tableception

--- data.py
+++ data.py
@@ -30,15 +30,14 @@ def get_description(path):
         html = request.read()
         soup = BeautifulSoup(html, 'html.parser')
 
+        content = soup.find_all(id = "pageContentWrapper")[0]
+        for string in content.stripped_strings:
+            if len(string) > 70:
+                return unicode(string).encode('utf-8')
+            
     except:
         print "Couldn't open: %s" % path
-        
-    content = soup.find_all(id = "pageContentWrapper")[0]
-    for string in content.stripped_strings:
-        if len(string) > 70:
-            return unicode(string).encode('utf-8')
-
-    return "No description provided."
+        return "No description provided."
   
 
 def lookup_courses_in_dep(path):
@@ -75,9 +74,40 @@ def lookup_courses_in_dep(path):
         except:
             print "something went wrong"
     """
-    table = soup.find_all("table")
+    table = soup.find_all('table')[-1]
 
-            
+    #print table
+
+    #print table.find_all('tr')    
+    for tr in table.find_all(tr_with_td):
+        #if first column has a link, get name and full course description by visiting link
+        #print tr
+        #print "\n"
+        tds = tr.find_all('td')
+        td1 = tds[0].find_all('a')
+        if td1:
+            name = extract_string(td1[0].stripped_strings)
+            course_url = urlparse.urljoin(path, td1[0]['href'])
+            descript = get_description(course_url)
+
+        else:
+            name = extract_string(tds[0])
+            descript = "No description available"
+        #becuz im lazy
+        misc = extract_string(tds[3].stripped_strings) if len(tds) > 3 else "None"
+        code = extract_string(tds[1].stripped_strings)
+
+        print name + " " + code
+        print misc
+        print descript
+        print "\n\n"
+       
+
+        
+
+def extract_string(iterable):
+    return " ".join([unicode(s) for s in iterable]).encode('utf-8')
+    
 def td_with_a(tag):
     """
     Custom function for filtering with BeautifulSoup. 
@@ -88,17 +118,17 @@ def td_with_a(tag):
     """
     return tag.name == 'td' and tag.contents and tag.contents[0].name == 'a'
 
-
+def tr_with_td(tag):
+    return tag.name == 'tr' and tag.find_all('td')
+    
 if __name__ == "__main__":
 
-    """
-    path = url % ('126957', '254108')
+   
+    path = "http://stuy.enschool.org/apps/pages/index.jsp?uREC_ID=126659&type=d&termREC_ID=&pREC_ID=253269"
     lookup_courses_in_dep(path)
 
-    path = url % ('126967', '359851')
-    get_description(path)
-    """
 
+    """
     for dept in depts:
         path = url % (dept['uREC_ID'], dept['pREC_ID'])
         if dept['pREC_ID']:
@@ -106,3 +136,4 @@ if __name__ == "__main__":
                 lookup_courses_in_dep(path)
             except:
                 print "this entire page is screwed"
+    """
