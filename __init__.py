987e8d361ae914d4188ad7e9a7373c9aea0257db
masha <maryged@gmail.com>
2016-06-15 16:03:09 -0400

    i s2g

--- __init__.py
+++ __init__.py
@@ -114,9 +114,9 @@ def rem_dep():
     utils.user_rem_dependency(request.form)
     return redirect(url_for('adder'))
 
+app.secret_key=os.urandom(32)
 if __name__=='__main__':
     #app.debug=True
-    app.secret_key=os.urandom(32)
     #print app.secret_key
     #app.run(host='0.0.0.0', port=5000)
     app.run()
